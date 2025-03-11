cad590370793f158ec5d3cacc32e7dffcb1dd0ae
IBRAHIM TAHA PINAR <tahapinar32@gmail.com>
2025-03-11 22:52:07 +0300

    Ex2

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -1,2 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543019
+# Your Name and Surname:IBRAHIM TAHA PINAR
+import sys
+print(f"Python version : {sys.version_info.major,sys.version_info.minor,sys.version_info.micro}")
+print(f"Version info : {sys.version_info}")
