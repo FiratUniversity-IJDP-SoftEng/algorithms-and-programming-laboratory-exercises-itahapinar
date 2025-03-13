c66699f12dc98dbe9d4e10f4c14eb725645a0e85
IBRAHIM TAHA PINAR <tahapinar32@gmail.com>
2025-03-13 15:34:35 +0300

    Ex7

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -1,2 +1,7 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543019
+# Your Name and Surname:IBRAHIM TAHA PINAR
+word = input("Please enter a string: ")
+listofWord = set(word)
+print("Character frequencies:")
+for letter in sorted(listofWord):
+    print(f"{letter} : {word.count(letter)}")
