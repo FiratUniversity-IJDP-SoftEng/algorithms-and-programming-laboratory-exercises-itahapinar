883f2ca9d96de541fb325461295c99b9520f5ef5
IBRAHIM TAHA PINAR <tahapinar32@gmail.com>
2025-03-13 15:57:08 +0300

    Ex8

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543019
+# Your Name and Surname:IBRAHIM TAHA PINAR
+highofPyramid = int(input("Please enter the high of pyramid :"))
+for i in range(1,(highofPyramid+1)):
+    print(" " * (highofPyramid-i) + "*" * (2 * i - 1))
