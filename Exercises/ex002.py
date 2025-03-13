f22a86570efe3bb4a421ec84123c3518447b1833
IBRAHIM TAHA PINAR <tahapinar32@gmail.com>
2025-03-13 17:58:03 +0300

    Ex2

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -2,4 +2,4 @@
 # Your Name and Surname:IBRAHIM TAHA PINAR
 import sys
 print(f"Python version : {sys.version_info.major,sys.version_info.minor,sys.version_info.micro}")
-print(f"Version info : {sys.version_info}")
+print(f"Version info : {sys.version_info}").
