9936d994ce88c1c8edffa13a46741809b1d31324
unknown <tahapinar32@gmail.com>
2025-03-17 16:51:00 +0300

    Ex1

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,5 +1,5 @@
 # Your Student ID:230543019 
-# Your Name and Surname:IBRAHIM TAHA PINA
+# Your Name and Surname:IBRAHIM TAHA PINAR
 print("""Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are
 """)
 
